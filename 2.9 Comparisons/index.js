179ec28418dc62dfeba9bcdebcddcd99b8ccb09a
ShoxruxNosirov <shoxruxfriend@gmail.com>
2023-01-27 15:09:27 +0500

    A file containing examples of comparison operations has been created

---
+++ 2.9 Comparisons/index.js
@@ -0,0 +1,10 @@
+console.log( 2 > 1 );  // true 
+console.log( 2 == 1 ); // false 
+console.log( 2 != 1 ); // true 
+
+console.log( 'Z' > 'A' ); // true
+console.log( 'Glow' > 'Glee' ); // true
+console.log( 'Bee' > 'Be' ); // true
+
+console.log( null === undefined ); // false
+console.log( null == undefined ); // true
