8c0f219c61f8c0d965b1d38f16d19795b6e18112
ShoxruxNosirov <shoxruxfriend@gmail.com>
2023-01-27 11:51:35 +0500

    In the created Date types folder, a file containing 8 primitive and one non-primitive data types was created

---
+++ 2.5 Data types/index.js
@@ -0,0 +1,26 @@
+let num = 25;
+console.log('number -> ', num);
+
+let str = 'hello world';
+console.log('stirng -> ', str);
+
+let bool = true;
+console.log('boolean -> ', bool);
+
+let bigInt_ = 25n;
+console.log('bigInt -> ', bigInt_);
+
+let spcial_type_null = null;
+console.log('null -> ', spcial_type_null);
+
+let spcial_type_undefined = undefined;
+console.log('undefined -> ', spcial_type_undefined);
+
+let obj = {
+    name: 'Shoxrux',
+    age: 24
+}
+console.log('object -> ', obj);
+
+let symbol = Symbol('new_symbol');
+console.log('symbol -> ', symbol.toString());
