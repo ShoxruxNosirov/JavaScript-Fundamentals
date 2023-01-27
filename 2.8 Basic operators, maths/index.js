6098e0f446333fa9b39c008e3313b5e0442aa496
ShoxruxNosirov <shoxruxfriend@gmail.com>
2023-01-27 15:01:45 +0500

    A file with some examples of mathematical operations has been created

---
+++ 2.8 Basic operators, maths/index.js
@@ -0,0 +1,12 @@
+console.log(37 - 13) // 14
+console.log(37 + 13) // 50
+console.log(37 * 13) // 481
+console.log(37 / 13) // 2.8461538461538463
+console.log(37 % 13) // 11
+console.log(37 ** 13) // 243569224216081300000  o'garuvchini saqlay oladigan qiymatidan oshib ketti
+
+let num = 2;
+console.log(num++) // 2 chiqadi num ni qiymati 3 ga o'zgaradi
+console.log(++num) // 4 chiqadi num ni qiymati 4 ga teng
+console.log(num--) // 4 chiqadi num ni qiymati 3 ga o'zgargan
+console.log(--num) // 2 chiqadi num ni qiymati 2 ga teng
