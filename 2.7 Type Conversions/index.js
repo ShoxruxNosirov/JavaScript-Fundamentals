f9cad9d4ae5281c2b2569374a09f55003331dd4a
ShoxruxNosirov <shoxruxfriend@gmail.com>
2023-01-27 14:50:52 +0500

    A sample file was created to change the variable types

---
+++ 2.7 Type Conversions/index.js
@@ -0,0 +1,13 @@
+let str = '1234';
+let num = Number(str);
+let num2 = +str;
+
+let bool = true;
+let str2 = String(bool);
+
+let num3 = 23;
+let bool1 = Boolean(num3);
+
+console.log(typeof str) // string
+console.log(typeof num) // number
+console.log(typeof bool) // boolean
