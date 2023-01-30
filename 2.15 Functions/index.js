4b2fd5453555d7384a4c7c6274aa35664db19cca
ShoxruxNosirov <shoxruxfriend@gmail.com>
2023-01-30 11:57:49 +0500

    Function Declaration

--- 2.15 Functions/index.js
+++ 2.15 Functions/index.js
@@ -5,7 +5,7 @@ function showMessage() {
 let userName = 'John';
 function showMessage() {
   let message = 'Hello, ' + userName;
-  alert(message);
+  console.log(message);
 }
 showMessage(); 
 
@@ -13,17 +13,17 @@ userName = 'John';
 function showMessage() {
   userName = "Bob"; // (1) changed the outer variable
   let message = 'Hello, ' + userName;
-  alert(message);
+  console.log(message);
 }
-alert( userName ); // John before the function call
+console.log( userName ); // John before the function call
 showMessage();
-alert( userName );
+console.log( userName );
 
 userName = 'John';
 function showMessage() {
   let userName = "Bob"; // declare a local variable
   let message = 'Hello, ' + userName; // Bob
-  alert(message);
+  console.log(message);
 }
 showMessage();
-alert( userName );
+console.log( userName );
